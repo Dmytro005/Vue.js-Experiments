33ae7043492d1b5eea6ea7a288cad9039d41639a
Dmtro Harazdovskiy <harazdovskiy@gmail.com>
2018-02-24 17:38:03 +0200

    Simple routing + params

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
-<template>
-    <div class="container">
-        <div class="row justify-content-center">
+  <template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Routing</h1>
+          <hr>
+          <app-header></app-header>
+          <hr>
+          <transition
+            enter-active-class="animated fadeInLeft"
+            mode="out-in"
+            >
             <router-view></router-view>
-        </div>
+          </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Header from './components/shared/Header.vue'
+
 export default {
+  components: {
+    appHeader: Header,
+  },
 };
 </script>
-
 <style>
 
 </style>
---
+++ src/components/shared/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link active-class='active' class="nav-item nav-link" to="/" exact>Home</router-link>
+    <router-link active-class='active' class="nav-item nav-link" to="/user">User</router-link>
+  </ul>
+</template>
+<script>
+  export default {
+    name: 'Header',
+  }
+</script>
+<style scoped>
+.active {
+  transition: 250ms all;
+  background-color: var(--primary);
+  color: white;
+  transform: scale(0.8);
+}
+</style>
